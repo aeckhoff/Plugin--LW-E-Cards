0520e5427ede83aa8881bbf0d05920f1c2524493
Dr. Andreas Eckhoff <eckhoff@logicnetwork.de>
2012-10-14 13:09:40 +0200

    added import of sub classes

--- lw_ecard.class.php
+++ lw_ecard.class.php
@@ -10,7 +10,16 @@ class lw_ecard extends lw_plugin
     
     public function buildPageOutput()
     {
-        return "E-Card";
+        if ($this->request->getRaw('hash')) {
+            include_once(dirname(__FILE__).'/classes/lwECardFrontend.php');
+            $class = new lwECardFrontend($this->request->getRaw('hash'));
+        }
+        else {
+            include_once(dirname(__FILE__).'/classes/lwECardBackend.php');
+            $class = new lwECardBackend();
+        }
+        $class->execute();
+        return $class->getOutput();
     }
     
 }
